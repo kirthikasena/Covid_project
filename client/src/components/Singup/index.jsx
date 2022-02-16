c1cb64ac29adc7b55326499aa2e3fd2e6421e11b
kirthika <kirthisenathiraja@gmail.com>
2022-02-16 14:33:47 +0530

    registration fixed

--- client/src/components/Singup/index.jsx
+++ client/src/components/Singup/index.jsx
@@ -39,70 +39,68 @@ const Signup = () => {
 	};
 
 	return (
-		
 		<div className={styles.signup_container}>
-			<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-			<img  src={im11} alt="im11" height="400" className={styles.start} />
-			<div className={styles.signup_form_container}>
-				<div className={styles.left}>
-				<h1>Create Account</h1>
-						<input
-							type="text"
-							placeholder="First Name"
-							name="firstName"
-							onChange={handleChange}
-							value={data.firstName}
-							required
-							className={styles.input}
-						/>
-						<input
-							type="text"
-							placeholder="Last Name"
-							name="lastName"
-							onChange={handleChange}
-							value={data.lastName}
-							required
-							className={styles.input}
-						/>
-						<input
-							type="email"
-							placeholder="Email"
-							name="email"
-							onChange={handleChange}
-							value={data.email}
-							required
-							className={styles.input}
-						/>
-						<input
-							type="password"
-							placeholder="Password"
-							name="password"
-							onChange={handleChange}
-							value={data.password}
-							required
-							className={styles.input}
-						/>
-						{error && <div className={styles.error_msg}>{error}</div>}
-						<button type="submit" className={styles.green_btn}>
-							Sign Up
-						</button>
-						<h5 style={{ color: 'white' }}>Already have an account? Log In</h5>
-					<Link to="/login">
-						<button type="button" className={styles.white_btn}>
-							Log in
-						</button>
-					</Link>
-				</div>
-				<div className={styles.right}>
-					<form className={styles.form_container} onSubmit={handleSubmit}>
-					
-				
-					</form>
-				</div>
+		<img  src={im11} alt="Quarantine period image" height="400" className={styles.start} />
+		<div className={styles.signup_form_container}>
+			<div className={styles.left}>
+			<h1>Create Account</h1>
+					<input
+						type="text"
+						placeholder="First Name"
+						name="firstName"
+						onChange={handleChange}
+						value={data.firstName}
+						required
+						className={styles.input}
+					/>
+					<input
+						type="text"
+						placeholder="Last Name"
+						name="lastName"
+						onChange={handleChange}
+						value={data.lastName}
+						required
+						className={styles.input}
+					/>
+					<input
+						type="email"
+						placeholder="Email"
+						name="email"
+						onChange={handleChange}
+						value={data.email}
+						required
+						className={styles.input}
+					/>
+					<input
+						type="password"
+						placeholder="Password"
+						name="password"
+						onChange={handleChange}
+						value={data.password}
+						required
+						className={styles.input}
+					/>
+					{error && <div className={styles.error_msg}>{error}</div>}
+					<button type="submit" className={styles.green_btn}>
+						Sign Up
+					</button>
+					<h5 style={{ color: 'white' }}>Already have an account? Log In</h5>
+				<Link to="/login">
+					<button type="button" className={styles.white_btn}>
+						Log in
+					</button>
+				</Link>
 			</div>
-			<img  src={im2} alt="im2" height="300" className={styles.covid} />
+			<div className={styles.right}>
+				<form className={styles.form_container} onSubmit={handleSubmit}>
+				
+			
+				</form>
+			</div>
 		</div>
-	);
+		<img  src={im2} alt="covid virus image" height="300" className={styles.covid} />
+	</div>
+);
 };
 
 export default Signup;
